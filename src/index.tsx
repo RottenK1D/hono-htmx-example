bd00d497cbc5a230d805ab53de85cbf9ab752503
Rinards Sondors <rinards.sondors@gmail.com>
2024-01-16 21:45:07 +0000

    Refactor index.tsx to use a Layout component
    This commit refactors the index.tsx file to use a Layout component for better code organization and reusability. It also introduces a Content component that renders the site's main content within the Layout.

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,50 @@
 import { Hono } from "hono";
+import { html } from "hono/html";
 import { FC } from "hono/jsx";
 
 const app = new Hono();
 
-interface Props {
-	name: string;
-	age: number;
+interface SiteData {
+	title: string;
+	description: string;
+	image: string;
+	children?: any;
 }
 
+const Layout = (props: SiteData) => html`
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>${props.title}</title>
+  <meta name="description" content="${props.description}">
+  <head prefix="og: http://ogp.me/ns#">
+  <meta property="og:type" content="article">
+  <!-- More elements slow down JSX, but not template literals. -->
+  <meta property="og:title" content="${props.title}">
+  <meta property="og:image" content="${props.image}">
+</head>
+<body>
+  ${props.children}
+</body>
+</html>
+`;
 
-
-const Home: FC<Props> = (props: Props) => {
-	return (
-		<div>
-			<h1>Hello Hono!</h1>
-			<div>{props.name}</div>
-			<div>{props.age}</div>
-		</div>
-	);
-};
+const Content = (props: { siteData: SiteData; name: string }) => (
+	<Layout {...props.siteData}>
+		<h1>Hello, {props.name}!</h1>
+	</Layout>
+);
 
 app.get("/", (c) => {
-	return c.html(<Home name={"Rinards"} age={22} />);
+	const props = {
+		name: "Hono",
+		siteData: {
+			title: "Hello World",
+			description: "This is a sample page.",
+			image: "https://example.com/image.png",
+		} as SiteData,
+	};
+	return c.html(<Content {...props} />);
 });
 
 export default app;
